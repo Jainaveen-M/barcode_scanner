92883b9a9a4add61c74820c7069b0057c13d0e42
jainaveen.m <jainaveen.m@kobil.com>
2021-07-27 20:06:15 +0530

    added dummy

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     Repo().getData();
-    print("init state");
+    print("init dummy state");
     super.initState();
   }
 
