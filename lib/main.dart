04b503a88f3fdd5e377fedf19643b51846d8771f
jainaveen.m <jainaveen.m@kobil.com>
2021-09-04 19:55:43 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cubit/repo.dart';
 import 'package:flutter/material.dart';
 
 //'http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=960f1b02257240a198f741b7784134b1'
@@ -6,7 +5,6 @@ void main() {
   runApp(MaterialApp(
     home: MyApp(),
   ));
-  Repo().getData();
 }
 
 class MyApp extends StatefulWidget {
@@ -18,56 +16,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  void initState() {
-    Repo().getData();
-    print("init dummy state");
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Counter Cubit"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            child: Text("Center"),
-          ),
-          Hero(
-            tag: 'lol',
-            child: Container(
-              child: Image.asset(
-                'assets/lollipop.png',
-                width: 100,
-                height: 100,
-              ),
-            ),
-          ),
-          Hero(
-            tag: "hello",
-            child: Container(
-              child: Text(
-                "Hello word flutter web!",
-                style: TextStyle(fontSize: 20),
-              ),
-              color: Colors.grey,
-              width: 200,
-              height: 100,
-            ),
-          ),
-          Container(
-            child: Text("Hellow world container"),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-    );
+    return Container();
   }
 }
